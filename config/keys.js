85138630a11f37e2e81ff56cb0a7f7f908c29ea8
Junaid Ehsan <junaid.ehsan@virtual-force.com>
2018-10-09 20:39:58 +0500

    Passport auth and validations

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://junaid:junaid12@ds223763.mlab.com:23763/devconnect'
+  mongoURI: 'mongodb://junaid:junaid12@ds223763.mlab.com:23763/devconnect',
+  secretOrKey: 'secret'
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const gravatar = require('gravatar') // Load gravatar
+const bcrypt = require('bcryptjs') // Load Bcrypt
+const User = require('../../models/User') // Load User model
+const jwt = require('jsonwebtoken') // Load JWT
+const keys = require('../../config/keys') // Load Keys
+const passport = require('passport') // Load passport
 
-// Load gravatar
-const gravatar = require('gravatar')
-
-// Load Bcrypt
-const bcrypt = require('bcryptjs')
-
-// Load User model
-const User = require('../../models/User')
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register')
 
 // @route  GET api/users/test
 // @desc   Tests user route
@@ -19,9 +19,18 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works!' }))
 // @desc   Register a user
 // @access Public
 router.post('/register', (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body)
+
+  console.log(isValid)
+  console.log(errors)
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors)
+  }
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: 'Email already exists!' })
+      errors.email = 'Email already exists'
+      return res.status(400).json(errors)
     } else {
       const avatar = gravatar.url(req.body.email, {
         size: 200, // Size
@@ -35,6 +44,8 @@ router.post('/register', (req, res) => {
         avatar,
         password: req.body.password
       })
+
+      console.log(newUser)
 
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
@@ -64,15 +75,45 @@ router.post('/login', (req, res) => {
       return res.status(404).json({ email: 'User not found!' })
     }
 
-    // Check passowrd
+    // Check password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: 'Success' })
+        // User Matched
+
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }
+
+        // Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            })
+          }
+        )
       } else {
         res.status(400).json({ password: 'Password incorrect!' })
       }
     })
   })
 })
+
+// @route  GET api/users/current
+// @desc   Return current user
+// @access Private
+router.get(
+  '/current',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
+    })
+  }
+)
 
 module.exports = router
