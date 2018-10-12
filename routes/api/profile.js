2b62fb468e1af3b462eafba4dcea39f61b540795
Junaid Ehsan <junaid.ehsan@virtual-force.com>
2018-10-12 19:11:59 +0500

    profile added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,65 @@
 const express = require('express')
 const router = express.Router()
+const mongoose = require('mongoose')
+const passport = require('passport')
+
+const Profile = require('../../models/Profile') // Load Profile model
+const User = require('../../models/User') // Load User model
 
 // @route  GET api/profile/test
 // @desc   Tests profile route
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'Profile Works!' }))
+
+// @route  GET api/profile
+// @desc   Get current user profile
+// @access Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {}
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user'
+          return res.status(404).json(errors)
+        }
+        res.json(Profile)
+      })
+      .catch(err => res.status(404).json(err))
+  }
+)
+
+// @route  Post api/profile
+// @desc   Create or Edit User Profile
+// @access Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    // Get Fields
+    const profileFields = {}
+    profileFields.user = req.user.id
+    if (req.body.handle) profileFields.handle = req.body.handle
+    if (req.body.company) profileFields.company = req.body.company
+    if (req.body.website) profileFields.website = req.body.website
+    if (req.body.location) profileFields.location = req.body.location
+    if (req.body.bio) profileFields.bio = req.body.bio
+    if (req.body.status) profileFields.status = req.body.status
+    if (req.body.githubusername) { profileFields.githubusername = req.body.githubusername }
+    // Skills - Split into array
+    if (typeof req.body.skills !== 'undefined') {
+      profileFields.skills = req.body.skills.split(',')
+    }
+    // Social
+    profileFields.social = {}
+    if (req.body.youtube) profileFields.social.youtube = req.body.youtube
+    if (req.body.twitter) profileFields.social.twitter = req.body.twitter
+    if (req.body.facebook) profileFields.social.facebook = req.body.facebook
+    if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin
+    if (req.body.instagram) profileFields.social.instagram = req.body.instagram
+  }
+)
 
 module.exports = router
