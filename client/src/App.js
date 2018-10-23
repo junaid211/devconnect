ded17a5f921b5d1a6d33be55a5949faf384fb26d
Junaid Ehsan <junaid.ehsan@virtual-force.com>
2018-10-23 20:47:40 +0500

    react router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
@@ -7,11 +8,13 @@ import './App.css'
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <Route exact path='/' component={Landing} />
+          <Footer />
+        </div>
+      </Router>
     )
   }
 }
