974b06746be0aaf8f3cce04ce3f16a742db2881b
Junaid Ehsan <junaid.ehsan@virtual-force.com>
2018-10-25 20:31:09 +0500

    login, register pages done

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
 import Landing from './components/layout/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 import './App.css'
 
 class App extends Component {
@@ -12,6 +14,10 @@ class App extends Component {
         <div className='App'>
           <Navbar />
           <Route exact path='/' component={Landing} />
+          <div className='container'>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </div>
           <Footer />
         </div>
       </Router>
